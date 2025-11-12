b44c2f2845f98ed9e9e983adcbf18e05ca435819
surelyagainb <rotche020607@gmail.com>
2025-11-12 14:24:22 +0800

    Add files via upload

---
+++ project-main/main/AssetSetter.java
@@ -0,0 +1,25 @@
+package main;
+
+import object.Obj_Chest;
+import object.Obj_Key;
+
+public class AssetSetter {
+
+    GamePanel gp;
+
+    public AssetSetter(GamePanel gp)
+    {
+        this.gp = gp;
+    }
+    
+    public void setObject(){
+    	gp.obj[0]= new Obj_Key();
+        gp.obj[0].worldX = 23 * gp.tileSize;
+        gp.obj[0].worldY = 7 * gp.tileSize;
+        
+        gp.obj[1] = new Obj_Chest();
+        gp.obj[1].worldX = 24 * gp.tileSize;
+        gp.obj[1].worldY = 8 * gp.tileSize;
+        
+    }
+}
---
+++ project-main/object/Obj_Chest.java
@@ -0,0 +1,17 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class Obj_Chest extends SuperObject{
+    
+    public Obj_Chest(){
+        name = "Chest";
+       try {
+        image = ImageIO.read(getClass().getResourceAsStream("/yes/chest.png"));
+       } catch (Exception e) {
+        e.printStackTrace();
+       }
+    }
+}
+
